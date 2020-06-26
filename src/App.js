9e36f605a48d868809e45c4b227f91408440c8d0
Sebastian <ascacere92@gmail.com>
2020-06-26 00:06:24 -0500

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const GIANT_NUMBER = 50000
+const generateRandomNames = () => {
+  return Array.from(Array(GIANT_NUMBER)).map(() => Math.ceil(Math.random() * 10))
+}
+
+const App = () => {
+  const [ name, setName ] = useState('');
+  const numbers = useMemo(() => () => generateRandomNames() , [])
+
+  const numbersJSX = numbers.map((n) => <div style={{ padding: '10px',}}>{n}</div>)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label for="name">Nombre: </label>
+        <input name="name" id="name" value={name} onChange={(evt) => {
+          setName(evt.target.value);
+        }} />
+      </form>
+      <div className="flex-container"
+           style={{
+             display: 'flex',
+             flexWrap: 'wrap'
+           }}>
+      { numbersJSX }
+    </div>
     </div>
   );
 }
